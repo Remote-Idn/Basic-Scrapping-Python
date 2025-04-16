4ff0ec15fca548cd34e54177d9ec939ae41db0d7
Rully-Rem84 <rsatriawan@gmail.com>
2025-04-16 23:15:02 +0700

    Tambah package

--- berita_terbaru_tribun/__init__.py
+++ berita_terbaru_tribun/__init__.py
@@ -1,6 +1,7 @@
 def ekstraksi_data():
     hasil = dict()
     hasil["judul"] = "Rekonstruksi Kasus 3 Polisi Meninggal Ditembak Kopda Basar Digelar Besok"
+    hasil["tanggal"] = "16 April 2025"
     hasil["waktu"] = "26 menit lalu"
     return hasil
 
@@ -8,4 +9,5 @@ def ekstraksi_data():
 def tampilkan_data(result):
     print("Berita Terbaru Tribun Lampung")
     print(f"Judul Berita: {result["judul"]}")
+    print(f"Tanggal Terbit: {result[tanggal]}")
     print(f"Waktu terbit: {result["waktu"]}")
