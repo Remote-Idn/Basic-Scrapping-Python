35b7f8c5f2e85b6e5360e4ed638bebecf2d18d7d
Rully-Rem84 <rsatriawan@gmail.com>
2025-04-22 11:39:43 +0700

    Tambah package

--- berita_terbaru_tribun/__init__.py
+++ berita_terbaru_tribun/__init__.py
@@ -1,13 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
 def ekstraksi_data():
+
+    try:
+        content = requests.get('https://www.cnnindonesia.com/tag/kebijakan-tarif-donald-trump')
+    except Exception:
+        return None
+
+    if content.status_code == 200:
+        print(content.status_code)
+    else:
+        return
+        print(content.status_code)
+
+    soup = BeautifulSoup(content.text, "html.parser")
+    content_tag = soup.find('div', {'class': "flex flex-col gap-5"})
+    if content.status_code == 200:
+        judul = soup.find('h2', {'class': "text-cnn_black_light dark:text-white mb-2 inline leading-normal text-xl group-hover:text-cnn_red"})
+        link = soup.find('a', {'href': "flex group items-center gap-4"})
+        waktu = soup.find('span', {'class': "text-xs text-cnn_black_light3"})
+    else:
+        print('Berita tidak ditemukan')
+
+
     hasil = dict()
-    hasil["judul"] = "Rekonstruksi Kasus 3 Polisi Meninggal Ditembak Kopda Basar Digelar Besok"
-    hasil["tanggal"] = "16 April 2025"
-    hasil["waktu"] = "26 menit lalu"
+    #hasil['berita'] = berita
+    hasil["judul"] = judul.text
+    hasil["link"] = link
+    hasil["waktu"] = waktu.text
     return hasil
 
-
 def tampilkan_data(result):
-    print("Berita Terbaru Tribun Lampung")
-    print(f"Judul Berita: {result["judul"]}")
-    print(f"Tanggal Terbit: {result["tanggal"]}")
-    print(f"Waktu terbit: {result["waktu"]}")
+    if result is None:
+        return 'pass'
+    print('Berita Terbaru CNN Indonesia')
+    print(f'{result["judul"]}')
+    print(f'{result["link"]}')
+    print(f'{result["waktu"]}')
+
+
+
