f692fa26c6f7bab79e7ad45800060822fe544f7b
Rully-Rem84 <rsatriawan@gmail.com>
2025-04-23 18:54:28 +0700

    Tambah package

--- berita_terbaru_tribun/__init__.py
+++ berita_terbaru_tribun/__init__.py
@@ -1,43 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def ekstraksi_data():
-
-    try:
-        content = requests.get('https://www.cnnindonesia.com/tag/kebijakan-tarif-donald-trump')
-    except Exception:
-        return None
-
-    if content.status_code == 200:
-        print(content.status_code)
+    #a = 1
+    #for page in range(0):
+    url = f'https://www.cnnindonesia.com'
+    ge = requests.get(url).text
+    soup = BeautifulSoup(ge, 'html.parser')
+    li = soup.find('div', {'class': "overflow-y-auto relative h-[322px]"})
+    lin = li.findAll('article')
+    for y in lin:
+            judul = y.find('h2', {'class': "text-base text-cnn_black_light group-hover:text-cnn_red"})
+            link = y.find('a'), {'href'}
+            #waktu = y.find('span', {'class': "text-xs text-cnn_black_light3"})
     else:
-        return
-        print(content.status_code)
+        print('Tidak ada data')
 
-    soup = BeautifulSoup(content.text, "html.parser")
-    content_tag = soup.find('div', {'class': "flex flex-col gap-5"})
-    if content.status_code == 200:
-        judul = soup.find('h2', {'class': "text-cnn_black_light dark:text-white mb-2 inline leading-normal text-xl group-hover:text-cnn_red"})
-        link = soup.find('a', {'href': "flex group items-center gap-4"})
-        waktu = soup.find('span', {'class': "text-xs text-cnn_black_light3"})
-    else:
-        print('Berita tidak ditemukan')
-
-
-    hasil = dict()
-    #hasil['berita'] = berita
-    hasil["judul"] = judul.text
-    hasil["link"] = link
-    hasil["waktu"] = waktu.text
-    return hasil
+        y = dict()
+        y["judul"] = judul.text
+        y["link"] = link
+        #y["waktu"] = waktu.text
+        return y
 
 def tampilkan_data(result):
-    if result is None:
-        return 'pass'
-    print('Berita Terbaru CNN Indonesia')
-    print(f'{result["judul"]}')
-    print(f'{result["link"]}')
-    print(f'{result["waktu"]}')
+        if result is None:
+            return 'pass'
+        print('Berita Terbaru CNN Indonesia')
+        print(f'{result["judul"]}')
+        print(f'{result["link"]}')
+        #print(f'{result["waktu"]}')
+
 
 
 
