1c813960bd33a725f95425824c94378d7844ae9b
Rully-Rem84 <rsatriawan@gmail.com>
2025-04-29 01:34:11 +0700

    Tambah package

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 '''
 Aplikasi Feed Berita
 '''
-import berita_terbaru_tribun
+import berita_terbaru_cnn
 
 if __name__ == "__main__":
     print("Aplikasi Utama")
-    result = berita_terbaru_tribun.ekstraksi_data()
-    berita_terbaru_tribun.tampilkan_data(result)
+    result = berita_terbaru_cnn.ekstraksi_data()
+    berita_terbaru_cnn.tampilkan_data(result)
